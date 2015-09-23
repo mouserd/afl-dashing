c5055b28f633e4b8e1991effa77a4fc8c622588b
David Mouser <David.Mouser@afl.com.au>
2015-09-23 20:58:48 +1000

    Updated get js test results from separate jenkins job rather than being 'extracted' from unit/integration test jobs.

--- jobs/jenkins/jenkins_tests.rb
+++ jobs/jenkins/jenkins_tests.rb
@@ -1,18 +1,9 @@
 
 def fetch_test_results
 
-  # We have to remove the JS test results from the unit tests as jenkins currently collects these together.
   unit_test_results = Jenkins.get_test_results("CMS unit test numbers")
-  integration_test_results = Jenkins.get_test_results("CMS integration test numbers");
-  js_unit_tests_results = Jenkins.get_js_test_results("CMS unit test numbers")
-
-  unit_test_results["current"] -= js_unit_tests_results["current"] || 0
-  unit_test_results["last"] -= js_unit_tests_results["last"] || 0
-
-  integration_test_results["current"] -= js_unit_tests_results["current"] || 0
-  integration_test_results["last"] -= js_unit_tests_results["last"] || 0
-
-  puts "[DEBUG] Recalculated unit test results #{unit_test_results}"
+  integration_test_results = Jenkins.get_test_results("CMS integration test numbers")
+  js_unit_tests_results = Jenkins.get_test_results("CMS unit test numbers (js)")
 
   send_event('unit-test-count', unit_test_results)
   send_event('js-unit-test-count', js_unit_tests_results)
